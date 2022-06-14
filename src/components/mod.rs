6ff75833e1e22d2bab242752e9fa0f8829530749
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-06-14 11:07:02 +0200

    components: Add `MapMarker`
    This component can be used as a marker on a `shumate::Map` and display
    any widget. The component has a special layouting which causes the
    component to be twice as high as its content. The content is always in
    the upper half. As a result, the bottom edge of the set widget of the
    component ends exactly at the geo position of the map.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,11 @@
 mod avatar;
+mod map_marker;
 mod message_entry;
 mod snow;
 mod sticker;
 
 pub(crate) use self::avatar::Avatar;
+pub(crate) use self::map_marker::MapMarker;
 pub(crate) use self::message_entry::MessageEntry;
 pub(crate) use self::snow::Snow;
 pub(crate) use self::sticker::Sticker;
