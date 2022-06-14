9cb9539bd51feddfaf7145ec9cb6b08d8ddfbefa
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2022-06-14 11:14:15 +0200

    components: Add `AvatarMapMarker`
    It is intended to embed this component in the previously created
    'MapMarker' component to be able to display a map marker with a user's
    avatar. This can be used for live locations.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,10 +1,12 @@
 mod avatar;
+mod avatar_map_marker;
 mod map_marker;
 mod message_entry;
 mod snow;
 mod sticker;
 
 pub(crate) use self::avatar::Avatar;
+pub(crate) use self::avatar_map_marker::AvatarMapMarker;
 pub(crate) use self::map_marker::MapMarker;
 pub(crate) use self::message_entry::MessageEntry;
 pub(crate) use self::snow::Snow;
